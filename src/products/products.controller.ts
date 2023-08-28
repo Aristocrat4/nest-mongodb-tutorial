e3d1c5ac20dd5b000f929d10282ed5283d18e3fb
Aristocrat4 <galadanelia1@gmail.com>
2023-08-28 15:18:35 +0400

    initial commit

---
+++ src/products/products.controller.ts
@@ -0,0 +1,62 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ProductsService } from './products.servise';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  async addProduct(
+    @Body('title') prodTitle: string,
+    @Body('description') prodDescription: string,
+    @Body('price') prodPrice: number,
+  ) {
+    const generatedId = await this.productsService.insertProduct(
+      prodTitle,
+      prodDescription,
+      prodPrice,
+    );
+    return { id: generatedId };
+  }
+
+  @Get()
+  async getAllProducts() {
+    const products = await this.productsService.getProducts();
+    return products;
+  }
+
+  @Get(':id')
+  getProduct(@Param('id') productId: string) {
+    return this.productsService.getSingleProduct(productId);
+  }
+
+  @Patch(':id')
+  async updateProduct(
+    @Param('id') prodId: string,
+    @Body('title') prodTitle: string,
+    @Body('description') prodDescription: string,
+    @Body('price') prodPrice: number,
+  ) {
+    await this.productsService.updateProduct(
+      prodId,
+      prodTitle,
+      prodDescription,
+      prodPrice,
+    );
+    return null;
+  }
+
+  @Delete(':id')
+  async removeProduct(@Param('id') prodId: string) {
+    await this.productsService.deleteProduct(prodId);
+    return null;
+  }
+}
---
+++ src/products/products.servise.ts
@@ -0,0 +1,83 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Product } from './product.model';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class ProductsService {
+  products: Product[] = [];
+
+  constructor(
+    @InjectModel('Product') private readonly productModel: Model<Product>,
+  ) {}
+
+  async insertProduct(title: string, desc: string, price: number) {
+    const newProduct = new this.productModel({
+      title: title,
+      description: desc,
+      price: price,
+    });
+    const result = await newProduct.save().then();
+    return result.id as string;
+  }
+
+  async getProducts() {
+    const products = await this.productModel.find().exec();
+    return products.map((prod) => ({
+      id: prod.id,
+      title: prod.title,
+      description: prod.description,
+      price: prod.price,
+    }));
+  }
+
+  async getSingleProduct(productId: string) {
+    const product = await this.findProduct(productId);
+    return {
+      id: product.id,
+      title: product.title,
+      description: product.description,
+      price: product.price,
+    };
+  }
+
+  async updateProduct(
+    productId: string,
+    title: string,
+    description: string,
+    price: number,
+  ) {
+    const updatedProduct = await this.findProduct(productId);
+    if (title) {
+      updatedProduct.title = title;
+    }
+    if (description) {
+      updatedProduct.description = description;
+    }
+    if (price) {
+      updatedProduct.price = price;
+    }
+    updatedProduct.save();
+  }
+
+  async deleteProduct(prodId: string) {
+    const result = await this.productModel.deleteOne({ _id: prodId }).exec();
+    console.log(result);
+    if (result.deletedCount === 0) {
+      throw new NotFoundException("couldn't find");
+    }
+  }
+
+  private async findProduct(id: string): Promise<Product> {
+    let product;
+    try {
+      product = await this.productModel.findById(id);
+    } catch (error) {
+      throw new NotFoundException("couldn't find");
+    }
+    if (!product) {
+      throw new NotFoundException("couldn't find");
+    }
+    return product;
+  }
+}
